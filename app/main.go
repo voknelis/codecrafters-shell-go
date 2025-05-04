bad557d2b48c7716c70d1666d05fe4b5b2659d58
Pavel Silchenkov <pavel.silchenkov13@gmail.com>
2025-05-04 21:12:52 +0300

    REPL

--- app/main.go
+++ app/main.go
@@ -8,16 +8,19 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	// infinite loop implements REPL (Read-Eval-Print Loop)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
-		return
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+			return
+		}
+
+		command = strings.TrimSpace(command)
+		fmt.Println(command + ": command not found")
 	}
-
-	command = strings.TrimSpace(command)
-	fmt.Println(command + ": command not found")
 }
