7b1914f7eb04448646c8604a0a8414c95cfa9d1f
Pavel Silchenkov <pavel.silchenkov13@gmail.com>
2025-05-04 21:07:52 +0300

    handle invalid command

--- app/main.go
+++ app/main.go
@@ -4,11 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+		return
+	}
+
+	command = strings.TrimSpace(command)
+	fmt.Println(command + ": command not found")
 }
