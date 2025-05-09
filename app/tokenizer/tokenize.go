5dc9b3a0185e9ff5feb797936f3785ca1e641882
Pavel Silchenkov <pavel.silchenkov13@gmail.com>
2025-05-09 11:02:15 +0300

    refactor: tokenizer

--- app/tokenizer/tokenize.go
+++ app/tokenizer/tokenize.go
@@ -5,107 +5,123 @@ import (
 	"strings"
 )
 
-var backslashEscapedCharacters = []rune{'\\', '$', '"', '`', '\n'}
+type tokenizerState struct {
+	inSingleQuote bool
+	inDoubleQuote bool
+	isEscaped     bool
+	currentToken  strings.Builder
+	tokens        []string
+}
 
-// Shell Quoting Modes and Escape Behavior explained
-//
-// inside single quote:
-// - no escaping is performed at all
-// - every character between the single quotes is literal
-// - you cannot escape anything inside single quotes — not even another single quote
-//   except to include a single quote, you have to end the quote, escape it, and reopen:
-//   'It'\''s fine' -> It's fine
-//
-// inside double quote
-// - backslash (\) only escapes these four characters: $, `, ", \
-// 	 "Hello \$USER" -> Hello $USER
-//   "Hello \"World\"" -> Hello "World"
-//   "Slash: \\" -> Slash: \
-//
-// unquoted (bare words):
-// - the backslash (\) escapes the next character.
-// - prevents special characters from being interpreted (like space, *, $, etc.).
+const (
+	singleQuote = '\''
+	doubleQuote = '"'
+	backslash   = '\\'
+	space       = ' '
+)
+
+var escapedCharactersInDoubleQuotes = []rune{'\\', '$', '"', '`', '\n'}
 
 func Tokenize(input string) []string {
-	tokens := make([]string, 0)
-	currentToken := strings.Builder{}
-	inSingleQuote := false
-	inDoubleQuote := false
-	isBackslash := false
+	state := tokenizerState{
+		tokens: []string{},
+	}
 
 	for _, char := range input {
-		switch char {
-		case '\'':
-			if inDoubleQuote {
-				if isBackslash {
-					isBackslash = false
-					currentToken.WriteRune('\\')
-				}
-				currentToken.WriteRune(char)
-			} else if isBackslash && !inSingleQuote && !inDoubleQuote {
-				currentToken.WriteRune(char)
-				isBackslash = false
-			} else {
-				inSingleQuote = !inSingleQuote
-			}
-		case '"':
-			if inSingleQuote {
-				currentToken.WriteRune(char)
-			} else {
-				if isBackslash {
-					isBackslash = false
-					currentToken.WriteRune(char)
-				} else {
-					inDoubleQuote = !inDoubleQuote
-				}
-			}
-		case '\\':
-			if inDoubleQuote {
-				if isBackslash {
-					isBackslash = false
-					currentToken.WriteRune(char)
-				} else {
-					isBackslash = true
-				}
-			} else if inSingleQuote {
-				// supposed to throw error, but for not just write it
-				currentToken.WriteRune(char)
-			} else if !inSingleQuote && !inDoubleQuote {
-				isBackslash = true
-			} else {
-				currentToken.WriteRune(char)
-			}
-		case ' ':
-			if isBackslash && !inDoubleQuote {
-				currentToken.WriteRune(char)
-			} else if inSingleQuote || inDoubleQuote {
-				if isBackslash {
-					currentToken.WriteRune('\\')
-				}
-				currentToken.WriteRune(char)
-			} else if currentToken.Len() > 0 {
-				tokens = append(tokens, currentToken.String())
-				currentToken.Reset()
-			}
+		state.processChar(char)
+	}
 
-			if isBackslash {
-				isBackslash = false
-			}
-		default:
-			if inDoubleQuote && isBackslash && !slices.Contains(backslashEscapedCharacters, char) {
-				currentToken.WriteRune('\\')
-			}
-			if isBackslash {
-				isBackslash = false
-			}
-			currentToken.WriteRune(char)
+	state.flushToken()
+
+	return state.tokens
+}
+
+func (t *tokenizerState) processChar(char rune) {
+	switch {
+	case t.inSingleQuote:
+		t.processCharInSingleQuote(char)
+	case t.inDoubleQuote:
+		t.processCharInDoubleQuote(char)
+	default:
+		t.processUnquotedChar(char)
+	}
+}
+
+// inside single quote:
+//   - no escaping is performed at all
+//   - every character between the single quotes is literal
+//   - you cannot escape anything inside single quotes — not even another single quote
+//     except to include a single quote, you have to end the quote, escape it, and reopen:
+//     'It'\”s fine' -> It's fine
+func (t *tokenizerState) processCharInSingleQuote(char rune) {
+	if char == singleQuote {
+		t.inSingleQuote = false
+		return
+	}
+
+	t.currentToken.WriteRune(char)
+}
+
+// inside double quote
+//   - backslash (\) only escapes these four characters: $, `, ", \
+//     "Hello \$USER" -> Hello $USER
+//     "Hello \"World\"" -> Hello "World"
+//     "Slash: \\" -> Slash: \
+func (t *tokenizerState) processCharInDoubleQuote(char rune) {
+	if t.isEscaped {
+		if slices.Contains(escapedCharactersInDoubleQuotes, char) {
+			t.processEscapedChar(char)
+		} else {
+			t.isEscaped = false
+			t.currentToken.WriteRune('\\')
+			t.currentToken.WriteRune(char)
 		}
+		return
 	}
 
-	// flush the rest of the last token data
-	if currentToken.Len() > 0 {
-		tokens = append(tokens, currentToken.String())
+	switch char {
+	case backslash:
+		t.isEscaped = true
+	case doubleQuote:
+		t.inDoubleQuote = false
+	default:
+		t.currentToken.WriteRune(char)
 	}
 
-	return tokens
+}
+
+// unquoted (bare words):
+// - the backslash (\):
+//   - escapes the next character
+//   - prevents special characters from being interpreted (like space, *, $, etc.)
+func (t *tokenizerState) processUnquotedChar(char rune) {
+	if t.isEscaped {
+		t.processEscapedChar(char)
+		return
+	}
+
+	switch char {
+	case backslash:
+		t.isEscaped = true
+	case singleQuote:
+		t.inSingleQuote = true
+	case doubleQuote:
+		t.inDoubleQuote = true
+	case space:
+		t.flushToken()
+	default:
+		t.currentToken.WriteRune(char)
+	}
+}
+
+func (t *tokenizerState) processEscapedChar(char rune) {
+	t.currentToken.WriteRune(char)
+	t.isEscaped = false
+}
+
+func (t *tokenizerState) flushToken() {
+	if t.currentToken.Len() > 0 {
+		t.tokens = append(t.tokens, t.currentToken.String())
+		t.currentToken.Reset()
+	}
 }
